d27f828ee584a9f7e452aa4032f5c886901e1edc
Jarrod Overson <jsoverson@gmail.com>
2021-11-05 20:35:33 -0400

    restructuring and consolidating other docker builds
    Former-commit-id: e892a4b33e0b4c901857a56a464fd6fbeab8255b

--- docker/dev-env.sh
+++ docker/dev-env.sh
@@ -6,7 +6,7 @@ setup_env() {
   echo '[ -s "$HOME/.bashrc" ] && . "$HOME/.bashrc"' >> "$HOME/.bash_profile"
 
   cat <<-'eof' >> "$HOME/.bashrc"
-export DEV_COMMON="/home/$HOME/dev-common"
+export DEV_COMMON="$HOME/dev-common"
 export PATH="$DEV_COMMON/bin:$PATH"
 
 eof
