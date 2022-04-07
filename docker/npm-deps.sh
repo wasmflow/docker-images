a9881af30c7ce35061b6abeea837f6dde98b28b2
Jarrod Overson <jsoverson@gmail.com>
2022-04-07 09:52:12 -0400

    Updated rust to 1.59.0 and moved the codegen build from the dev branch to main

--- docker/npm-deps.sh
+++ docker/npm-deps.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 install_npm_deps() {
   # npm install -g @vinodotdev/codegen
-  git clone -b dev https://github.com/vinodotdev/codegen.git
+  git clone https://github.com/vinodotdev/codegen.git
   cd codegen && npm install && npm run build && rm -rf node_modules && NODE_ENV=production npm install && npm install -g .
 }
 
