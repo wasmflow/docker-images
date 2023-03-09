aa598eda408f57d87b29b53f3de86fc36e3f78bc
Jarrod Overson <jsoverson@gmail.com>
2023-03-09 11:56:33 -0500

    added just to rust-base

--- docker/rust-base.sh
+++ docker/rust-base.sh
@@ -11,6 +11,7 @@ install_rust_deps() {
   cargo install tomlq cargo-deny just
   cargo install --git https://github.com/wasmflow/cross.git --branch darwin
   rustup +nightly component add rustfmt
+  rustup +nightly target add wasm32-wasi
 }
 
 setup_rcfiles() {
